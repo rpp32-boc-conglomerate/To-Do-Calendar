6b91faaeddb42145ed86d0af8a0edb494cbe096e
Chelsea Lipson <chelseamlipson@gmail.com>
2022-04-03 18:57:52 -0700

    Created sample event.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import moment from 'moment';
 import Registration from './components/authentication/registration.jsx';
 import MyCalendar from './components/calendar/MyCalendar.jsx';
 import './CalendarStyle.scss'
@@ -7,14 +8,20 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      currentPage: 'home'
+      currentPage: 'home',
+      events: [{
+        title: 'Sample Event',
+        start: new Date,
+        end: new Date(moment().add(1, 'hour')),
+        allDay: false
+      }]
     }
   }
 
   render() {
     return (
       <>
-        <MyCalendar />
+        <MyCalendar myEventsList={this.state.events}/>
         <Registration />
         <div />
       </>
