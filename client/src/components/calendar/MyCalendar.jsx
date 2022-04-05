f2a72b25ac838948c1c9d1c6ab2b4a6072dacac9
Chelsea Lipson <chelseamlipson@gmail.com>
2022-04-04 19:58:43 -0700

    Enabled drag and drop and event resizing within the calendar.

--- client/src/components/calendar/MyCalendar.jsx
+++ client/src/components/calendar/MyCalendar.jsx
@@ -1,21 +1,56 @@
-import React from 'react';
+import React, { Fragment, useCallback, useMemo, useState } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import './CalendarStyle.scss';
+import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
 
+const DragAndDropCalendar = withDragAndDrop(Calendar)
 const localizer = momentLocalizer(moment);
 
-const MyCalendar = (props) => (
-  <div className='calendar'>
-    <Calendar
+const MyCalendar = (props) => {
+  const [myEvents, setMyEvents] = useState(props.myEventsList);
+
+  const moveEvent = useCallback(
+    ({ event, start, end, isAllDay: droppedOnAllDaySlot = false }) => {
+      const { allDay } = event
+      if (!allDay && droppedOnAllDaySlot) {
+        event.allDay = true
+      }
+
+      setMyEvents((prev) => {
+        const existing = prev.find((ev) => ev.id === event.id) ?? {}
+        const filtered = prev.filter((ev) => ev.id !== event.id)
+        return [...filtered, { ...existing, start, end, allDay }]
+      })
+    },
+    [setMyEvents]
+  )
+
+  const resizeEvent = useCallback(
+    ({ event, start, end }) => {
+      setMyEvents((prev) => {
+        const existing = prev.find((ev) => ev.id === event.id) ?? {}
+        const filtered = prev.filter((ev) => ev.id !== event.id)
+        return [...filtered, { ...existing, start, end }]
+      })
+    },
+    [setMyEvents]
+  )
+
+  return (
+    <div className='calendar'>
+    <DragAndDropCalendar
       localizer={localizer}
       defaultView="week"
-      events={props.myEventsList}
+      events={myEvents}
       startAccessor="start"
       endAccessor="end"
+      onEventDrop={moveEvent}
+      onEventResize={resizeEvent}
       style={{ height: 500 }}
     />
   </div>
-)
+  )
+}
 
 export default MyCalendar;
