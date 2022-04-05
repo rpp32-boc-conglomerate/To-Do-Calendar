76a72fb35c42198e9b5f188a4d8388b1159869a3
Rob Stafford <uwillgo2themoon@gmail.com>
2022-04-05 09:04:27 -0700

    Build db & tables

---
+++ database/index.js
@@ -0,0 +1,94 @@
+require ('dotenv').config()
+const pg = require('pg');
+const pgtools = require('pgtools');
+
+const config = {
+  user: 'postgres',
+  password: process.env.PGPASS,
+  port: 5432,
+  host: 'localhost'
+}
+
+pgtools.createdb(config, 'tododb', function (err, res) {
+  if (err) {
+    console.error(err);
+    process.exit(-1);
+  }
+  console.log(res);
+  addTables();
+});
+
+let addTables = function() {
+
+  const db = new pg.Pool({
+    host: 'localhost',
+    database: 'tododb',
+    user: 'postgres',
+    password: process.env.PGPASS,
+    port: '5432'
+   });
+
+   const execute = async (query) => {
+    try {
+        await db.connect();     // gets connection
+        await db.query(query);  // sends queries
+        return true;
+    } catch (error) {
+        console.error(error.stack);
+        return false;
+    }
+  };
+
+  const queryText1 = `
+      CREATE TABLE IF NOT EXISTS "users" (
+        "id" SERIAL,
+        "name" VARCHAR(64) NOT NULL,
+        "hash" VARCHAR(64) NOT NULL,
+        PRIMARY KEY ("id")
+      );`;
+
+  execute(queryText1)
+    .then(result => {
+      if (result) {
+          console.log('user table created: ', result);
+      }
+    })
+
+  const queryText2 = `
+    CREATE TABLE IF NOT EXISTS "categories" (
+      "id" SERIAL,
+      "name" VARCHAR(64) NOT NULL,
+      PRIMARY KEY ("id")
+    );`;
+
+  execute(queryText2)
+  .then(result => {
+    if (result) {
+        console.log('category table created: ', result);
+    }
+  })
+
+  const queryText3 = `
+    CREATE TABLE IF NOT EXISTS "todoItems" (
+      "id" SERIAL,
+      "itemName" VARCHAR(64) NOT NULL,
+      "itemDesc" VARCHAR(255) NOT NULL,
+      "duration" INTERVAL NOT NULL,
+      "startDay" DATE NOT NULL,
+      "startTime" TIME NOT NULL,
+      "userID" INTEGER NOT NULL,
+      "catagoryID" INTEGER NOT NULL,
+      PRIMARY KEY ("id")
+    );`;
+
+  execute(queryText3)
+  .then(result => {
+    if (result) {
+        console.log('to do item table created: ', result);
+        db.end();
+    }
+  })
+}
+
+
+
