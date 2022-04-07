8da3cd843abafcef6c74a268034a97f18379641c
a244629128 <gjyyl9394967@gmail.com>
2022-04-07 00:39:42 -0400

    registration encryption

--- server/routes/registrationRouter.js
+++ server/routes/registrationRouter.js
@@ -1,7 +1,9 @@
 const registrationRouter = require('express').Router();
 var cors = require('cors');
-
-// registrationRouter.options('*', cors());
+const mongoDB = require('../../database/mongoDB.js');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+registrationRouter.options('*', cors());
 
 registrationRouter.route('/').get((req, res) => {
   console.log('get signup route');
@@ -10,9 +12,36 @@ registrationRouter.route('/').get((req, res) => {
 
 
 
+// registrationRouter.route('/').post((req, res) => {
+//   console.log('post signup route');
+//   console.log(req.body);
+//   res.sendStatus(200);
+// });
+
 registrationRouter.route('/').post((req, res) => {
-  console.log('post signup route');
-  console.log(req.body);
-  res.sendStatus(200);
+  console.log('register page:', req.body);
+  bcrypt.hash(req.body.password, saltRounds, async (err, hash) => {
+    const newUser = new mongoDB.userModel({
+      username: req.body.username,
+      password: hash
+    });
+    let email = await mongoDB.userModel.findOne({ username: req.body.username });
+    console.log('check existing email:', email === null);
+    if (email === null) {
+      newUser.save((err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('account save into db')
+          res.send(true);
+        }
+      })
+    } else {
+      res.send('existed')
+    }
+  })
+
 });
+
+
 module.exports = registrationRouter;
