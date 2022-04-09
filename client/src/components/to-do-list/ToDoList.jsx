b3c93e865713794d11f48869b34d7dceb44d6349
merwhite11 <meredith.white91@gmail.com>
2022-04-08 20:52:38 -0700

    Merge with peter's file

--- client/src/components/to-do-list/ToDoList.jsx
+++ client/src/components/to-do-list/ToDoList.jsx
@@ -2,15 +2,7 @@ import React, {useState, useEffect} from 'react';
 import Categories from './Categories.jsx';
 import Category from './Category.jsx';
 import Tasks from './Tasks.jsx';
-<<<<<<< HEAD
-import Drop from './testDrop.jsx';
-import TestModal from './testModal.jsx';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-import { Container, Grid, ButtonGroup, Button } from '@material-ui/core';
-=======
 import { Button } from '@material-ui/core';
->>>>>>> 995817ae3ec71ecad2ffbae479cc5105163ca4ec
 
 function ToDoList() {
   //a state prop that's an array that has an element for everytime + task or + category is clicked
@@ -18,43 +10,6 @@ function ToDoList() {
   const [newCategories, setNewCategories] = useState([])
   const [touched, setTouch] = useState(false)
 
-<<<<<<< HEAD
-  function handleAddTask(index) {
-    console.log('add task category index', index)
-    if (newTasks[index]) {
-      setNewTasks(newTasks => newTasks[index].concat('New'))
-    } else {
-      let newTask = {}
-      newTask = {index: 'new'}
-      setNewTasks(newTasks => ({
-        ...newTasks,
-        ...newTask
-      }))
-    }
-    console.log('newTasks', newTasks)
-}
-
-  function openModal() {
-    console.log('openModal called')
-    setTouch(true)
-  }
-
-  return(
-    <Container>
-    <DndProvider backend={HTML5Backend}>
-    <Grid item xs={2} md={4}>To-Do List
-    <ButtonGroup variant="contained" aria-label="outlined primary button group">
-      <Button onClick={() => setNewCategories(newCategories => newCategories.concat('New'))}>+ Category</Button>
-      <Button onClick={() => setNewTasks(newTasks => newTasks.concat('New task'))}>+ Task</Button>
-    </ButtonGroup>
-      </Grid>
-      <Categories categories={newCategories} tasks={newTasks} addTask={handleAddTask} openModal={openModal}/>
-      {/* <Tasks tasks={newTasks} openModal={openModal} /> */}
-      <Drop tasks={newTasks}/>
-      {touched && <TestModal/>}
-    </DndProvider>
-    </Container>
-=======
   var deleteTask = (e) => {
     // var target = e.target;
     // console.log(target);
@@ -75,7 +30,6 @@ function ToDoList() {
         {/* <Tasks tasks={newTasks} /> */}
       </div>
     </div>
->>>>>>> 995817ae3ec71ecad2ffbae479cc5105163ca4ec
   )
 }
 //don't forget empty array parameter for useEffect !!!
