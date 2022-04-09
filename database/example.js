45164938a12962c96522b69aa24c785293e9f261
Peter Park <mac-main@Peters-MBP-2.lan>
2022-04-08 20:37:20 -0700

    Fix package files

--- database/example.js
+++ database/example.js
@@ -24,8 +24,20 @@ module.exports = {
   //   end: new Date(moment().add(1, 'hour')),
   //   allDay: false
   // }]
-
+{
+  id: '1'
+  category: 'travel',
   tasks: [
+    {
+      id: 1,
+      title: 'Trip to China',
+      description: '5-day business trip to meet with manufacturers',
+      duration: '5 days',
+      start: 'April 9, 2022 09:30:00',
+      end: 'April 13, 2022 09:30:00',
+      category_id: 1
+      inCalendar: true
+    },
     {
       id: 1,
       title: 'Trip to China',
@@ -37,12 +49,38 @@ module.exports = {
     },
     {
       id: 1,
+      title: 'Trip to China',
+      description: '5-day business trip to meet with manufacturers',
+      duration: '5 days',
+      start: 'April 9, 2022 09:30:00',
+      end: 'April 13, 2022 09:30:00',
+      category_id: 1
+    },
+  ]
+}
+
+
+
+  tasks: [
+    {
+      id: 1,
+      title: 'Trip to China',
+      description: '5-day business trip to meet with manufacturers',
+      duration: '5 days',
+      start: 'April 9, 2022 09:30:00',
+      end: 'April 13, 2022 09:30:00',
+      category_id: 1,
+      inCalendar: true
+    },
+    {
+      id: 1,
       title: 'Trip to Los Angeles',
       description: 'Meeting with Executives',
       duration: '6 hours',
       start: 'April 14, 2022 09:30:00',
       end: 'April 14, 2022 15:30:00',
-      category_id: 1
+      category_id: 1,
+      inCalendar: false
     },
     {
       id: 1,
@@ -51,7 +89,8 @@ module.exports = {
       duration: '3 hours',
       start: 'April 17, 2022 11:30:00',
       end: 'April 17, 2022 14:30:00',
-      category_id: 2
+      category_id: 2,
+      inCalendar: false
     },
     {
       id: 1,
@@ -60,7 +99,8 @@ module.exports = {
       duration: '2 hours',
       start: 'April 15, 2022 09:30:00',
       end: 'April 15, 2022 11:30:00',
-      category_id: 2
+      category_id: 2,
+      inCalendar: true
     },
     {
       id: 1,
@@ -69,7 +109,8 @@ module.exports = {
       duration: '2 hours',
       start: 'April 16, 2022 13:30:00',
       end: 'April 16, 2022 15:30:00',
-      category_id: 2
+      category_id: 2,
+      inCalendar: false
     },
     {
       id: 1,
@@ -78,7 +119,8 @@ module.exports = {
       duration: '30 minutes',
       start: 'April 14, 2022 18:30:00',
       end: 'April 14, 2022 19:00:00',
-      category_id: 3
+      category_id: 3,
+      inCalendar: false
     },
     {
       id: 1,
@@ -87,7 +129,8 @@ module.exports = {
       duration: '2 hours',
       start: 'April 15, 2022 15:30:00',
       end: 'April 15, 2022 17:30:00',
-      category_id: 3
+      category_id: 3,
+      inCalendar: false
     },
     {
       id: 1,
@@ -97,6 +140,7 @@ module.exports = {
       start: 'April 15, 2022 18:30:00',
       end: 'April 13, 2022 22:30:00',
       category_id: 4
+      inCalendar: false
     },
   ]
 }
