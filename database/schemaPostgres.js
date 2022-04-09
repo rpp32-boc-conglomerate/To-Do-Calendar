8fc9ba1e8c0d3815170701ec22e1fc54ea4a8564
Rob Stafford <uwillgo2themoon@gmail.com>
2022-04-09 13:17:28 -0700

    Changes to db to facilitate current/future functions, rename schema file

--- database/schemaPostgres.js
+++ database/schemaPostgres.js
@@ -49,6 +49,13 @@ let addTables = function() {
     }
   };
 
+  const queryText1 = `
+    CREATE TABLE IF NOT EXISTS "users" (
+      "user_id" INT GENERATED ALWAYS AS IDENTITY,
+      "email" VARCHAR(64) NOT NULL,
+      PRIMARY KEY ("user_id")
+    );`;
+
   const queryText2 = `
     CREATE TABLE IF NOT EXISTS "categories" (
       "category_id" INT GENERATED ALWAYS AS IDENTITY,
@@ -57,29 +64,49 @@ let addTables = function() {
     );`;
 
   const queryText3 = `
-    CREATE TABLE IF NOT EXISTS "todoItems" (
-      "todo_id" SERIAL,
-      "itemName" VARCHAR(64) NOT NULL,
-      "itemDesc" VARCHAR(255) NOT NULL,
+    CREATE TABLE IF NOT EXISTS "todoitems" (
+      "id" SERIAL,
+      "title" VARCHAR(64) NOT NULL,
+      "description" VARCHAR(255) NOT NULL,
       "duration" INTERVAL NOT NULL,
-      "startDay" DATE NOT NULL,
-      "startTime" TIME NOT NULL,
+      "start" TIMESTAMP NOT NULL,
+      "end" TIMESTAMP NOT NULL,
       "inCalendar" BOOLEAN NOT NULL,
-      "user_id" INTEGER NOT NULL,
       "category_id" INTEGER NOT NULL,
-      PRIMARY KEY ("todo_id"),
+      PRIMARY KEY ("id"),
       CONSTRAINT fk_category
         FOREIGN KEY("category_id")
           REFERENCES categories("category_id")
     );`;
 
-    execute(queryText2)
+    const queryText4 = `
+    CREATE TABLE IF NOT EXISTS "userToDoLookup" (
+      "user_id" INT NOT NULL,
+      "todo_id" INT NOT NULL,
+      "owner" BOOLEAN NOT NULL,
+      CONSTRAINT fk_user
+        FOREIGN KEY("user_id")
+          REFERENCES users("user_id"),
+      CONSTRAINT fk_todo
+        FOREIGN KEY("todo_id")
+          REFERENCES todoitems("id")
+    );`;
+
+    execute(queryText1)
+      .then(function(result) {
+        console.log('user table created: ', result);
+        return execute(queryText2);
+      })
       .then(function(result) {
         console.log('category table created: ', result);
-        return execute(queryText3)
+        return execute(queryText3);
       })
       .then(function(result) {
         console.log('to do item table created: ', result);
+        return execute(queryText4);
+      })
+      .then(function(result) {
+        console.log('lookup table created: ', result);
         db.end();
       })
 }
