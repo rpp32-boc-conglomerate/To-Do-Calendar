bd0ce9991d2932bbdeee137390e0c902032bcb77
Peter Park <mac-main@Peters-MBP-2.lan>
2022-04-11 01:29:00 -0700

    Organized todoList styling and setup, and hooked up example data

--- client/src/components/to-do-list/Task.jsx
+++ client/src/components/to-do-list/Task.jsx
@@ -1,32 +1,33 @@
-import React, {useEffect} from 'react';
-import {useDrag} from 'react-dnd';
+import React, { useEffect, useState } from 'react';
+import { useDrag } from 'react-dnd';
 import "./styles.scss";
-import { makeStyles, Container, Grid, Card, ButtonGroup, Button, TextareaAutosize } from '@material-ui/core';
+import { makeStyles, Container, Grid, Card, ButtonGroup, Button, TextareaAutosize, TextField } from '@material-ui/core';
 
 const TaskOptionsModal = require('../TaskOptionsModal.jsx');
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles({
   textArea: {
-    padding: '2rem',
-    width: '40vw',
+    padding: '1rem',
+    width: '90%',
     color: 'blue'
   },
   card: {
+    display: 'flex',
     border: '2rem solid black',
   }
-}))
+});
 
 
-//drag div , need to call hook
-//isDragging returns t or f
-//drag reference which element you want to make draggable
-//every element requires a type
-function Task(props) {
-  //the object is what you'll be passing in drop
+// drag div, need to call hook
+// isDragging returns t or f
+// drag reference which element you want to make draggable
+// every element requires a type
+var Task = (props) => {
+  // the object is what you'll be passing in drop
   const classes = useStyles();
+
   const [{isDragging}, drag] = useDrag(() => ({
     type: "task",
-    id: props.index,
     item: {id: props.index},
     //define different states/props, but optional
 
@@ -37,6 +38,8 @@ function Task(props) {
 
   const [isOpen, setIsOpen] = useState(false);
 
+  console.log('Task', props.task);
+
   return (
     <Card
       id="task"
@@ -45,16 +48,35 @@ function Task(props) {
       className={classes.card}
       // style={{border: isDragging ? "2px solid pink" : "0px"}}
     >
-      <TextareaAutosize
-        aria-label="empty textarea"
-        placeholder="Empty"
+      <TextField
+        disabled
+        defaultValue={props.task.title}
+        className={classes.textArea}
+      />
+      <TextField
+        disabled
+        defaultValue={props.task.duration}
         className={classes.textArea}
       />
       <Button variant="contained" onClick={() => setIsOpen()}>Edit</Button>
       <Button variant="contained" onClick={() => console.log('Task X Button Clicked!')}>X</Button>
-      { isOpen === false && <TaskOptionsModal open={isOpen}/> }
+      {/* { isOpen === false && <TaskOptionsModal open={isOpen}/> } */}
     </Card>
   );
 };
 
 export default Task;
+
+// function DraggableComponent(props) {
+//   const [collected, drag, dragPreview] = useDrag(() => ({
+//     type,
+//     item: { id }
+//   }))
+//   return collected.isDragging ? (
+//     <div ref={dragPreview} />
+//   ) : (
+//     <div ref={drag} {...collected}>
+//       ...
+//     </div>
+//   )
+// }
--- client/src/components/to-do-list/Tasks.jsx
+++ client/src/components/to-do-list/Tasks.jsx
@@ -5,7 +5,12 @@ import Task from './Task.jsx';
 
 function Tasks({tasks, editTask, deleteTask}) {
   return(
-    tasks.map((task, i) => <Task key={i} index={i} title={task.title} deleteTask={deleteTask}/>)
+    tasks.map((task, i) => {
+      if (task.inCalendar === false) {
+        console.log('TASKS task: ', task);
+        return (<Task key={i} index={i} title={task.title} task={task} deleteTask={deleteTask}/>);
+      }
+    })
   )
 }
 
