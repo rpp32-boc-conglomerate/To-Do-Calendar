26bc80de46801bd53e92946b94efaa3c9c633164
Peter Park <mac-main@Peters-MBP-2.lan>
2022-04-22 21:16:53 -0700

    Finished button functionalities

--- client/src/components/calendar/MyCalendar.jsx
+++ client/src/components/calendar/MyCalendar.jsx
@@ -4,11 +4,20 @@ import { makeStyles, Container, Button } from '@material-ui/core';
 import moment from 'moment';
 import './CalendarStyle.scss';
 import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
+import TaskOptionsModal from '../TaskOptionsModal.jsx';
 
 const DragAndDropCalendar = withDragAndDrop(Calendar);
 const localizer = momentLocalizer(moment);
 
 const MyCalendar = (props) => {
+  const [todo, setTodo] = useState({});
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleOnSelectEvent = (event) => {
+    setTodo(event);
+    setModalOpen(true);
+  }
+
   if (props.viewingShared) {
     return (
       <DragAndDropCalendar
@@ -26,23 +35,28 @@ const MyCalendar = (props) => {
   }
 
   return (
-    <DragAndDropCalendar
-      className='calendar'
-      localizer={localizer}
-      defaultView="week"
-      events={props.formatForCalendar(props.myEvents)? props.formatForCalendar(props.myEvents).filter(item => item.in_calendar) : []}
-      startAccessor="start"
-      endAccessor="end_date"
-      onSelectEvent={(event) => {
-        props.changeTitle(event);
-      }}
-      min={new Date(moment().hour(6).minute(0))}
-      max={new Date(moment().hour(23).minute(0))}
-      onEventDrop={props.moveEvent}
-      onEventResize={props.resizeEvent}
-      onDropFromOutside={props.onDropFromOutside}
-      style={{ height: 1000 }}
-    />
+    <>
+      <DragAndDropCalendar
+        className='calendar'
+        localizer={localizer}
+        defaultView="week"
+        events={props.formatForCalendar(props.myEvents)? props.formatForCalendar(props.myEvents).filter(item => item.in_calendar) : []}
+        startAccessor="start"
+        endAccessor="end_date"
+        onSelectEvent={(event) => {
+          handleOnSelectEvent(event);
+        }}
+        min={new Date(moment().hour(6).minute(0))}
+        max={new Date(moment().hour(23).minute(0))}
+        onEventDrop={props.moveEvent}
+        onEventResize={props.resizeEvent}
+        onDropFromOutside={props.onDropFromOutside}
+        style={{ height: 1000 }}
+      />
+      {modalOpen === true && <TaskOptionsModal setModalOpen={setModalOpen} modalOpen={modalOpen} task={todo}
+      addTodo={props.addTodo} updateTodo={props.updateTodo}
+      deleteTodo={props.deleteTodo} newTodo={false}/>}
+    </>
   )
 }
 
--- client/src/components/to-do-list/Categories.jsx
+++ client/src/components/to-do-list/Categories.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Category from './Category.jsx';
 
-function Categories({isMobile, taskData, draggedEvent, setDraggedEvent, handleDragStart, addTodo, updateTodo, deleteTodo}) {
+function Categories({isMobile, taskData, draggedEvent, setDraggedEvent, handleDragStart, addTodo, updateTodo, deleteTodo, deleteCategory}) {
   return (
     taskData.length ? taskData.map((category, i) => {
       return <Category key={i}
-        tasks={category}
+        tasks={category} deleteCategory={deleteCategory}
         isMobile={isMobile} draggedEvent={draggedEvent}
         setDraggedEvent={setDraggedEvent} handleDragStart={handleDragStart}
         addTodo={addTodo} updateTodo={updateTodo} deleteTodo={deleteTodo}/>
